15f4cbc24528204772f9069bd9588b5af2fc05e1
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-11-13 18:35:12 +0000

    More things

--- ncsdns.py
+++ ncsdns.py
@@ -99,26 +99,36 @@ acache = dict([(DomainName(ROOTNS_DN),
 # [domain name --> CnameCacheEntry]
 cnamecache = dict([])
 
-def update_cnamecache(cname_records, ref_time):
-  for record in cname_records:
-    domain, cname, ttl = record._dn, record._cname, record._ttl
+# Updates nscache with the given NS records.
+# Does not overwrite entries with greater TTL.
+def update_nscache(ns_records, ref_time):
+  for record in ns_records:
+    domain, nsdn, ttl = record._dn, record._nsdn, record._ttl
     if ttl == 0: continue # do not cache records with 0 TTL
-    new_entry = CnameCacheEntry(cname,
-                                expiration=ttl + long(ref_time),
-                                authoritative=True)
+    new_entry = CacheEntry(expiration=ttl + long(ref_time),
+                           authoritative=True)
+    if domain not in nscache:
+      nscache[domain] = OrderedDict([(nsdn, new_entry)])
+    elif nsdn not in nscache[domain] or \
+         nscache[domain][nsdn]._expiration < new_entry._expiration:
+      nscache[domain][nsdn] = new_entry
 
-    if domain not in cnamecache or \
-       cnamecache[domain]._expiration < new_entry._expiration:
-      cnamecache[domain] = new_entry
-
-def get_cnamecache(domain, ref_time):
+# Returns the NS records in nscache matching the given domain (and have
+# not expired). The returned records have an adjusted TTL according to
+# the elapsed time since they were inserted.
+def get_nscache(domain, ref_time):
   results = []
-  if domain in cnamecache:
-    entry = cnamecache[domain]
-    if entry._expiration > ref_time:
-      results.append(RR_CNAME(domain, entry._expiration - long(ref_time), entry._cname))
+  while len(results) == 0:
+    if domain in nscache:
+      for nsdn, entry in nscache[domain].iteritems():
+        if entry._expiration > ref_time:
+          results.append(
+            RR_NS(domain, entry._expiration - long(ref_time), nsdn))
+    domain = domain.parent()
   return results
 
+# Updates acache with the given A records.
+# Does not overwrite entries with greater TTL.
 def update_acache(a_records, ref_time):
   for record in a_records:
     domain, ttl = record._dn, record._ttl
@@ -133,35 +143,42 @@ def update_acache(a_records, ref_time):
          acache[domain]._dict[addr]._expiration < new_entry._expiration:
       acache[domain]._dict[addr] = new_entry
 
+# Returns the A records in acache matching the given domain (and have
+# not expired). The returned records have an adjusted TTL according to
+# the elapsed time since they were inserted.
 def get_acache(domain, ref_time):
   results = []
   if domain in acache:
     for addr, entry in acache[domain]._dict.iteritems():
       if entry._expiration > ref_time:
-        results.append(RR_A(domain, entry._expiration - long(ref_time), addr.toNetwork()))
+        results.append(
+          RR_A(domain, entry._expiration - long(ref_time), addr.toNetwork()))
   return results
 
-def update_nscache(ns_records, ref_time):
-  for record in ns_records:
-    domain, nsdn, ttl = record._dn, record._nsdn, record._ttl
+# Updates cnamecache with the given CNAME records.
+# Does not overwrite entries with greater TTL.
+def update_cnamecache(cname_records, ref_time):
+  for record in cname_records:
+    domain, cname, ttl = record._dn, record._cname, record._ttl
     if ttl == 0: continue # do not cache records with 0 TTL
-    new_entry = CacheEntry(expiration=ttl + long(ref_time),
-                           authoritative=True)
+    new_entry = CnameCacheEntry(cname,
+                                expiration=ttl + long(ref_time),
+                                authoritative=True)
 
-    if domain not in nscache:
-      nscache[domain] = OrderedDict([(nsdn, new_entry)])
-    elif nsdn not in nscache[domain] or \
-         nscache[domain][nsdn]._expiration < new_entry._expiration:
-      nscache[domain][nsdn] = new_entry
+    if domain not in cnamecache or \
+       cnamecache[domain]._expiration < new_entry._expiration:
+      cnamecache[domain] = new_entry
 
-def get_nscache(domain, ref_time):
+# Returns the CNAME records in cnamecache matching the given domain (and have
+# not expired). The returned records have an adjusted TTL according to
+# the elapsed time since they were inserted.
+def get_cnamecache(domain, ref_time):
   results = []
-  while len(results) == 0:
-    if domain in nscache:
-      for nsdn, entry in nscache[domain].iteritems():
-        if entry._expiration > ref_time:
-          results.append(RR_NS(domain, entry._expiration - long(ref_time), nsdn))
-    domain = domain.parent()
+  if domain in cnamecache:
+    entry = cnamecache[domain]
+    if entry._expiration > ref_time:
+      results.append(
+        RR_CNAME(domain, entry._expiration - long(ref_time), entry._cname))
   return results
 
 # Parse the command line and assign us an ephemeral port to listen on:
@@ -193,32 +210,32 @@ sys.stdout.flush()
 setdefaulttimeout(TIMEOUT)
 cs = socket(AF_INET, SOCK_DGRAM)
 
-# generates a random ID for header (0-65535)
+# Returns a random 16-bit ID for DNS message header (0-65535)
 def rand_id():
-  return randint(0, (1 << 16) - 1)
+  return randint(0, (1 << 16) - 1) # 1 << 16 == 2**16
 
-# determines whether the first domain name is the same as OR a child of the second
-def is_part_of(child, parent):
+# Determines whether the first domain name is the same as OR
+# a child of the second.
+def is_subdomain_of(child, parent):
   while child != None:
     if child == parent:
       return True
     child = child.parent()
   return False
 
-def flatten(lst):
-  return [item for sublist in lst for item in sublist]
-
-
-def get_an_ns_from_cache(domain, ref_time):
+# For a given domain, it returns a dictionary of:
+# - the cached A and CNAME records as "answers"
+# - the NS records as "nameservers"
+def get_cache(domain, ref_time):
   answers = get_acache(domain, ref_time) + get_cnamecache(domain, ref_time)
   nameservers = get_nscache(domain, ref_time)
-  additional = get_glue_records(nameservers, [], ref_time)
   return {
     "answers": answers,
-    "nameservers": nameservers,
-    "additional": additional
+    "nameservers": nameservers
   }
 
+# Given an RR (A, CNAME or NS), it returns a tuple of 3 items that uniquely TODO
+# Used to identify duplicate records.
 def tuple_from_record(record):
   last = None
   if hasattr(record, "_addr"):
@@ -229,7 +246,11 @@ def tuple_from_record(record):
     last = record._cname
   return (record._type, record._dn, last)
 
+# Given a list of records (A, CNAME or NS), it removes duplicates, keeping the
+# ones with the highest TTL.
 def discard_dup_records(records):
+  # sorting by descending TTL ensures that any duplicates occurring will be of
+  # lower or equal TTL.
   records = sorted(records, key=lambda x: x._ttl, reverse=True)
   seen = set()
   results = []
@@ -240,6 +261,8 @@ def discard_dup_records(records):
       seen.add(t)
   return results
 
+# Given a domain and a list of NS records, it returns the highest qualified NS
+# records for the domain.
 def get_highest_qualified(domain, ns_records):
   results = []
   while len(results) == 0 and domain != DomainName("."):
@@ -255,11 +278,10 @@ def get_glue_records(ns_records, a_records, ref_time):
     results += glue_records
   return discard_dup_records(results)
 
-def get_glued(ns_records, a_records, ref_time):
+def get_glued(ns_records, a_records):
   results = []
   for ns in ns_records:
-    glue_records = filter(lambda x: ns._nsdn == x._dn, a_records) \
-                 + get_acache(ns._nsdn, ref_time)
+    glue_records = filter(lambda x: ns._nsdn == x._dn, a_records)
     results.append((ns, glue_records))
   return sorted(results, key=lambda x: len(x[1]), reverse=True)
 
@@ -279,7 +301,7 @@ def ask_server(domain, dns_ip):
 
   print "\n=============================================================================\n"
 
-  print "Query to %s:" % dns_ip
+  print "QUERY TO %s:\n" % dns_ip
   print message
 
   cs.sendto(message_data, (dns_ip, DNS_PORT))
@@ -295,7 +317,7 @@ def ask_server(domain, dns_ip):
     logger.log(DEBUG2, "Ignored corrupted message.")
     return None
 
-  print "\nResponse:"
+  print "\nRESPONSE:\n"
   print response
 
   # discard packets received with mismatching id
@@ -317,26 +339,32 @@ def resolver(domain, begin=None, answers=None, nameservers=None, additional=None
   if nameservers is None: nameservers = []
   if additional is None: additional = []
 
-  cache = get_an_ns_from_cache(domain, ref_time=begin)
+  cache = get_cache(domain, ref_time=begin)
 
   answers = discard_dup_records(answers + cache["answers"])
   nameservers = discard_dup_records(nameservers + cache["nameservers"])
-  additional = discard_dup_records(additional + cache["additional"])
+  additional = get_glue_records(nameservers, additional, ref_time=begin)
 
-  if aggregate is None: aggregate = {
-    "answers": answers,
-    "nameservers": nameservers,
-    "additional": additional
-  }
+  if aggregate is None:
+    aggregate = {
+      "answers": answers,
+      "nameservers": nameservers,
+      "additional": additional
+    }
+  else:
+    dict_append(aggregate, { "answers": answers,
+                             "nameservers": nameservers,
+                             "additional": additional })
 
   answer_a_records =     filter(lambda x: isinstance(x, RR_A), answers)
   answer_cname_records = filter(lambda x: isinstance(x, RR_CNAME), answers)
 
   if answer_a_records:
+    aggregate_answers = discard_dup_records(aggregate["answers"])
     highest_qualified = get_highest_qualified(domain, aggregate["nameservers"])
     glue_records = get_glue_records(highest_qualified, additional, ref_time=begin)
     return {
-      "answers": aggregate["answers"],
+      "answers": aggregate_answers,
       "nameservers": highest_qualified,
       "additional": glue_records
     }
@@ -348,7 +376,7 @@ def resolver(domain, begin=None, answers=None, nameservers=None, additional=None
         q["answers"] = [record] + q["answers"]
         return q
 
-  for ns, a_records_for_ns in get_glued(nameservers, additional, ref_time=begin):
+  for ns, a_records_for_ns in get_glued(nameservers, additional):
 
     if not a_records_for_ns:
       dns_query = resolver(ns._nsdn, begin=begin)
@@ -385,6 +413,7 @@ def query(domain, begin, dns_ips=[], aggregate=None):
     if time() > (begin + MAX_QUERY_TIME):
       raise ExceededMaxQueryTime
     try:
+      print ith_dns_ip
       dns_ip = dns_ips[ith_dns_ip]
       response = ask_server(domain, dns_ip)
       if response is None:
@@ -399,7 +428,7 @@ def query(domain, begin, dns_ips=[], aggregate=None):
   answer_records =       filter(lambda x: domain == x._dn, response.answers)
   answer_a_records =     filter(lambda x: isinstance(x, RR_A), answer_records)
   answer_cname_records = filter(lambda x: isinstance(x, RR_CNAME), answer_records)
-  ns_records =           filter(lambda x: isinstance(x, RR_NS) and is_part_of(domain, x._dn), response.nameservers)
+  ns_records =           filter(lambda x: isinstance(x, RR_NS) and is_subdomain_of(domain, x._dn), response.nameservers)
   additional_a_records = filter(lambda x: isinstance(x, RR_A), response.additional)
 
   update_acache(answer_a_records + additional_a_records, ref_time=begin)
@@ -417,7 +446,7 @@ def query(domain, begin, dns_ips=[], aggregate=None):
 while 1:
   (data, address) = ss.recvfrom(512) # DNS limits UDP msgs to 512 bytes
   if not data:
-    log.error("client provided no data")
+    log.error("Client provided no data.")
     continue
 
   reply = ""
@@ -425,17 +454,17 @@ while 1:
   question = d.question
 
   if not question:
+    log.error("Client provided no question.")
     continue
 
   try:
     q = resolver(question._dn)
   except ExceededMaxQueryTime:
     q = None
-    logger.log(DEBUG2, "Exceeded maximum query time.")
+    log.error("Exceeded maximum query time.")
   except ExceededMaxRetries:
     q = None
-    logger.log(DEBUG2, "Excedded maximum retries.")
-
+    log.error("Exceeded maximum retries.")
 
   if q:
     message = Message(question=question, **q)
